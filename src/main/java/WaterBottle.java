51dc868b40a923aed802f83fa111f9592792e9ce
Chris Barclay <chris.barclay1@gmail.com>
2023-04-10 18:19:18 +0100

    WaterBottle Completed

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,27 @@
 public class WaterBottle {
+
+    private int volume;
+
+    public WaterBottle(int volume){
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void drink(){
+        setVolume(this.volume - 10);
+    }
+
+    public void empty(){
+        setVolume(0);
+    }
+
+    public void fill(){
+        setVolume(100);
+    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+    @Before
+    public void setup(){
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, waterBottle.getVolume());
+    }
+
+    @Test
+    public void canDrink(){
+        waterBottle.drink();
+        waterBottle.drink();
+        assertEquals(80, waterBottle.getVolume());
+    }
+
+    @Test
+    public void canEmpty(){
+        waterBottle.empty();
+        assertEquals(0, waterBottle.getVolume());
+    }
+
+    @Test
+    public void canFill(){
+        waterBottle.fill();
+        assertEquals(100, waterBottle.getVolume());
+    }
 }
