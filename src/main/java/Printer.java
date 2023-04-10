5d5e5f0196312a467a55f574a424c283b75438b1
Chris Barclay <chris.barclay1@gmail.com>
2023-04-10 20:14:56 +0100

    Printer Completed

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,32 @@
 public class Printer {
+
+    private int paperRemaining;
+    private int tonerVolume;
+
+    public Printer(int paperRemaining, int tonerVolume){
+        this.paperRemaining = paperRemaining;
+        this.tonerVolume = tonerVolume;
+    }
+
+    public void print(int pages, int copies){
+        int total = pages * copies;
+        if(total <= this.paperRemaining && total <= this.tonerVolume ){
+            setPaperRemaining(this.paperRemaining - total);
+            setTonerVolume(this.tonerVolume - total);
+        }
+    }
+
+    public int getPaperRemaining() {
+        return this.paperRemaining;
+    }
+    public void setPaperRemaining(int paperRemaining) {
+        this.paperRemaining = paperRemaining;
+    }
+
+    public int getTonerVolume() {
+        return tonerVolume;
+    }
+    public void setTonerVolume(int tonerVolume) {
+        this.tonerVolume = tonerVolume;
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,3 +1,39 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void setup(){
+        printer = new Printer(100, 15);
+    }
+
+    @Test
+    public void hasPaperRemaining(){
+        assertEquals(100, printer.getPaperRemaining());
+    }
+
+    @Test
+    public void hasTonerRemaining(){
+        assertEquals(15, printer.getTonerVolume());
+    }
+
+    @Test
+    public void canPrint() {
+        printer.print(2, 5);
+        assertEquals(90, printer.getPaperRemaining());
+        assertEquals(5, printer.getTonerVolume());
+    }
+
+    @Test
+    public void cannotPrint(){
+        printer.print(10, 5);
+        assertEquals(100, printer.getPaperRemaining());
+        assertEquals(15, printer.getTonerVolume());
+    }
 
 }
