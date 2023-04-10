2c5655708712ae52a8c9a69fa7ec1eb4233bb1c1
Chris Barclay <chris.barclay1@gmail.com>
2023-04-10 18:02:37 +0100

    Calculator Completed

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,18 @@
 public class Calculator {
+
+    public int add(int first, int second){
+        return first + second;
+    }
+
+    public int subtract(int first, int second){
+        return first - second;
+    }
+
+    public int multiply(int first, int second){
+        return first * second;
+    }
+
+    public double divide(int first, int second){
+        return ((double)first / (double)second);
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void canAdd(){
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(10, calculator.subtract(15, 5));
+    }
+
+    @Test
+    public void canMultiply(){
+        assertEquals(99, calculator.multiply(11, 9));
+    }
+
+    @Test
+    public void canDivideWholeNumbers(){
+        assertEquals(3, calculator.divide(12, 4), 0.01);
+//        assertEquals(2.5, calculator.divide(10, 4));
+    }
+    @Test
+    public void canDivideDoubleNumbers(){
+//        assertEquals(3, calculator.divide(12, 4));
+        assertEquals(2.5, calculator.divide(10, 4), 0.01);
+    }
+
+
 }
